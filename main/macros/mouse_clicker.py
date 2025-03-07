64e7c8b154e46cb67193077b6594944fcc5e0dce
Amos Sng <sngamos9@gmail.com>
2025-03-06 23:07:52 -0500

    adjusted cubing loop timing

--- main/macros/mouse_clicker.py
+++ main/macros/mouse_clicker.py
@@ -27,11 +27,11 @@ def blind_cubing_loop(kb_listener,mouse_controller,keyboard_controller):
             print("Stop key pressed, exiting loop.")
             break
         cubing_click(kb_listener,mouse_controller)
-        random_pauser(kb_listener,0.2,0.5)
+        random_pauser(kb_listener,0.3,0.6)
         keyboard_controller.press(Key.enter)
-        random_pauser(kb_listener,0.2,0.5)
+        random_pauser(kb_listener,0.3,0.6)
         keyboard_controller.release(Key.enter)
-        random_pauser(kb_listener,0.2,0.5)
+        random_pauser(kb_listener,0.3,0.6)
         keyboard_controller.press(Key.enter)
         random_pauser(kb_listener,1.5,1.7)
     return 1
